91949c1e24f0e36e9f29272186406153b8ba8dde
grro <gregor.roth@1und1.de>
2019-09-15 10:11:42 +0200

    repo renamed

--- train.sh
+++ train.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# define pipeline version to use
+# define pipeline to train
 groupId="eu.redzoo.ml"
 artifactId="pipeline-houseprice"
 version="1.0.3"
 
-echo "loading pipeline component $groupId/$artifactId/$version"
-pipeline_app_uri="https://github.com/grro/ml_deploy/blob/master/repo/lib-releases/""${groupId//.//}""/""${artifactId//.//}""/$version/""${artifactId//.//}""-$version-jar-with-dependencies.jar?raw=true"
-echo $pipeline_app_uri
+echo "loading copmoents"
+pipeline_app_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/lib-releases/""${groupId//.//}""/""${artifactId//.//}""/$version/""${artifactId//.//}""-$version-jar-with-dependencies.jar?raw=true"
 curl -L $pipeline_app_uri > pipeline.jar
 
-echo "loading ingest component"
-ingest_app_uri="https://github.com/grro/ml_deploy/blob/master/repo/lib-releases/eu/redzoo/ml/ingest-housedata/2.2.3/ingest-housedata-2.2.3-jar-with-dependencies.jar?raw=true"
+ingest_app_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/lib-releases/eu/redzoo/ml/ingest-housedata/2.2.3/ingest-housedata-2.2.3-jar-with-dependencies.jar?raw=true"
 curl -L $ingest_app_uri > ingest.jar
 
-echo "loading house data"
 train_data="https://github.com/grro/ml_deploy/blob/master/src/test/resources/train.csv?raw=true"
 curl -L $train_data > train.csv
 
@@ -27,10 +24,12 @@ trained=$artifactId-$version-$train_version".ser"
 java -jar pipeline.jar $trained
 
 echo  "upload trained pipeline"
-model_uri="https://github.com/grro/ml_deploy/blob/master/repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version$train_version/$trained
+model_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained
 echo "uploading $model_uri"
 
-#rm pipeline.jar
-#rm ingest.jar
-#rm artifact.info
-#rm train.csv
+rm $trained
+rm pipeline.jar
+rm ingest.jar
+rm train.csv
+rm labels.json
+rm records.json
