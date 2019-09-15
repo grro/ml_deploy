8e38763612040168d1d0397c30cc2fbab69e960b
grro <gregor.roth@1und1.de>
2019-09-15 09:31:31 +0200

    script optimizations

--- train.sh
+++ train.sh
@@ -13,15 +13,24 @@ echo "loading ingest component"
 ingest_app_uri="https://github.com/grro/ml_deploy/blob/master/repo/lib-releases/eu/redzoo/ml/ingest-housedata/2.2.3/ingest-housedata-2.2.3-jar-with-dependencies.jar?raw=true"
 curl -L $ingest_app_uri > ingest.jar
 
-
-source_data="module-ingest-housedata/src/test/resources/train.csv"
+echo "loading house data"
+train_data="https://github.com/grro/ml_deploy/blob/master/train.csv?raw=true"
+curl -L $train_data > train.csv
 
 echo "perform ingest component"
-java -jar ingest.jar $source_data
+java -jar ingest.jar train.csv
 
 echo "create and train pipeline"
 java -jar pipeline.jar
 
 echo  "package and upload trained pipeline"
+
+"https://github.com/grro/ml_deploy/blob/master/repo/lib-releases/""${groupId//.//}""/""${artifactId//.//}""/$version/""${artifactId//.//}""-$version-jar-with-dependencies.jar?raw=true"
+
 echo "groupId=$groupId&artifactId=$artifactId&version=$version" > "artifact.info"
 tar cfv $groupId"_"$artifactId"_"$version"_"$now.tar artifact.info trainedstate.ser
+
+rm pipeline.jar
+rm ingest.jar
+rm artifact.info
+rm train.csv
