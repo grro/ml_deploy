a54f91f9e76be43aca95a4a879171a90a149294c
grro <gregor.roth@1und1.de>
2019-09-16 11:05:26 +0200

    build process updated

--- build_rest_server.sh
+++ build_rest_server.sh
@@ -11,10 +11,6 @@ cd build
 
 
 
-echo "copying $artifactId jar to local dir"
-trained_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained"
-curl -s -L $trained_uri --output trained.ser
-
 
 # build server
 git clone https://github.com/grro/ml_deploy.git
@@ -27,7 +23,13 @@ echo $new_pom > pom.xml
 
 mvn clean install
 
-java -jar target/module-framework-rest-1.0-SNAPSHOT.jar --estimatorFilename=/mnt/c/workspace/ml_deploy/pipeline-houseprice-1.0.3-1568611516.ser
+
+echo "copying $artifactId jar to local dir"
+trained_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained
+curl -s -L $trained_uri --output trained.ser
+
+
+java -jar target/module-framework-rest-1.0-SNAPSHOT.jar --estimatorFilename=trained.ser
 
 
 # download
