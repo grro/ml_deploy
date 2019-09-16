4923055ba8372689e0e89a3d237b8480a56e4b57
grro <gregor.roth@1und1.de>
2019-09-16 18:20:12 +0200

    comments improved

--- src/main/scripts/build_rest_server_image.sh
+++ src/main/scripts/build_rest_server_image.sh
@@ -11,26 +11,28 @@ cd build
 
 
 # build server
-git clone https://github.com/grro/ml_deploy.git
+echo "copying framework-rest source to local dir"
+git clone --quiet https://github.com/grro/ml_deploy.git
 cd ml_deploy/module-framework-rest
 
-pom=$(<pom.xml)
-additional_dependency="<dependency><groupId>"$groupId"</groupId><artifactId>"$artifactId"</artifactId><version>"$version"</version></dependency>"
-new_pom=${pom/"<!-- PLACEHOLDER -->"/$additional_dependency}
-echo $new_pom > pom.xml
-
-echo "copying $artifactId jar to local dir"
+echo "download trained $artifactId pipeline to framework-rest/src/main/resources dir"
 trained=$artifactId-$version-$train_version".ser"
 trained_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained"?raw=true"
-echo $trained_uri
 curl -s -L $trained_uri --output trained.ser
 cd src/main
 mkdir resources
 cd ../..
 mv trained.ser src/main/resources
 
+echo "adding $artifactId to framework-rest pom.xml file "
+pom=$(<pom.xml)
+additional_dependency="<dependency><groupId>"$groupId"</groupId><artifactId>"$artifactId"</artifactId><version>"$version"</version></dependency>"
+new_pom=${pom/"<!-- PLACEHOLDER -->"/$additional_dependency}
+echo $new_pom > pom.xml
+
+
 echo "build rest server jar including the specific pipeline artifacts"
-mvn clean install
+mvn -q clean install
 
 cp target/module-framework-rest-1.0.3.jar ../../rest_server.jar
 cd ../../..
