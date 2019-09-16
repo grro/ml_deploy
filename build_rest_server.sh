c47fe712dbab7daead2a371c8dd18aed99d34edb
grro <gregor.roth@1und1.de>
2019-09-16 10:47:30 +0200

    trained estimator added

--- build_rest_server.sh
+++ build_rest_server.sh
@@ -3,11 +3,17 @@
 # define pipeline to train
 groupId="eu.redzoo.ml"
 artifactId="pipeline-houseprice"
-version="1.0.3"
+version="1.0-SNAPSHOT"
 
 
 mkdir build
 cd build
+
+
+
+echo "copying $artifactId jar to local dir"
+trained_uri="https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained"
+curl -s -L $trained_uri --output trained.ser
 
 
 # build server
@@ -21,10 +27,10 @@ echo $new_pom > pom.xml
 
 mvn clean install
 
-
+java -jar target/module-framework-rest-1.0-SNAPSHOT.jar --estimatorFilename=/mnt/c/workspace/ml_deploy/pipeline-houseprice-1.0.3-1568611516.ser
 
 
 # download
-# downloading   "uploadin trained pipeline https://github.com/grro/ml_deploy/blob/master/example-repo/model-releases/"${groupId//.//}/${artifactId//.//}/$version-$train_version/$trained
+# downloading   "uploadin trained pipeline
 
 
